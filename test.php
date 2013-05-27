f95535bf6a83a552db9d0a18b489f7c168b9a7bb
tao.lu <tao.lu@mixi.cn>
2013-05-27 17:10:34 +0800

    change varchar => text

--- test.php
+++ test.php
@@ -2,7 +2,13 @@
 require_once('db/DbAdapter.php');
 
 $db = new DbAdapter();
-$result = $db->select_user_by_email("a@bb.com");
+$result = $db->select_user_by_email("a@b.com");
 var_dump($result);
 echo get_magic_quotes_gpc();
+
+/*
+
+
+*/
+
 ?>
