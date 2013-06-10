e6d1a099ae539112c1f302052f1658b7775c9362
guoli.chen <guoli.chen@mixiu.cn>
2013-06-11 01:26:15 +0800

    Modify company_profile.php job_applications.php -> company_profile.php

--- company/company_profile.php
+++ company/company_profile.php
@@ -4,11 +4,15 @@
     require_once "$_SERVER[DOCUMENT_ROOT]/lib/MySmarty.php";
     define('COMPANY_PROFILE_TPL', 'company/company_profile.tpl');
 
-    $company_id = require_login();
-    $company = try_get_company_detail($company_id);
-    $job_applications = try_get_job_applications($company_id);
-    
-    show_company_profile($company, $job_applications);
+    $company_id = session_get('company_id');
+    if ($company_id) {
+        $company = try_get_company_detail($company_id);
+        $job_applications = try_get_job_applications($company_id);
+        
+        show_company_profile($company, $job_applications);
+    } else {
+        redirect('/company/login.php');
+    }
     
     function try_get_company_detail($company_id) {
         $res = get_company_detail_api($company_id);
